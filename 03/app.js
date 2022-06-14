aa12566c67a4473da18b49d98d4248908bfcaa67
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-06-14 18:29:43 +0200

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,30 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
+
+    const btnEl = document.querySelector('button')
+    const spanEl = document.querySelector('span')
+
+    btnEl.addEventListener(
+
+        'click',
+        function () {
+
+            const promise = fetch('https://api.ipify.org/')
+
+            promise
+                .then(resp => resp.text())
+                .then(ip => spanEl.innerText = ip)
+                .catch(err => console.log(err))
+                .finally(() => {
+
+                    console.log('Odpytywanie API zakończone')
+
+                })
+
+        }
+
+    )
+    
+
 }
